78deeebfd77a6102221b0c83ac381f413352c812
chenmh <chenmh@sucsoft.com>
2022-03-11 16:34:15 +0800

    tag: 修改文件目录结构及调整打包配置, 可实现多个子项目切换

--- gh-page/packages/vite-react-ts/vite.config.ts
+++ gh-page/packages/vite-react-ts/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/vite-react-ts',
-    plugins: [ react() ]
+    plugins: [ react() ],
+    build: {
+        // 子项目必须打包成库模式, 且入口文件名为 [main.js] 供主项目调用
+        lib: {
+            entry: resolve(__dirname, './src/main.ts'),
+            formats: [ 'es' ],
+            fileName: () => 'main.js'
+        }
+    }
 })
