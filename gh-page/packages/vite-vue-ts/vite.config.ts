4482e766a66455f0a0fd58a298e85a343da0a7e0
ChenMH <2036604698@qq.com>
2022-03-12 23:31:35 +0800

    fix: suffix of entry

--- gh-page/packages/vite-vue-ts/vite.config.ts
+++ gh-page/packages/vite-vue-ts/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     build: {
         // 子项目必须打包成库模式, 且入口文件名为 [main.js] 供主项目调用
         lib: {
-            entry: resolve(__dirname, './src/main.ts'),
+            entry: resolve(__dirname, './src/main.tsx'),
             formats: [ 'es' ],
             fileName: () => 'main.js'
         }
