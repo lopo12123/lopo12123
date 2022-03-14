25227364fd3a9323eefd77b3bd1f062732bca66c
lopo <lopo@zju.edu.cn>
2022-03-14 14:41:45 +0800

    style: animation fade-in and fade-out

--- gh-page/src/main.tsx
+++ gh-page/src/main.tsx
@@ -2,6 +2,7 @@
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "@/styles/animations.scss";
 
 // core
 import { StrictMode } from "react";
