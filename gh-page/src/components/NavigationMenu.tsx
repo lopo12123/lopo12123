4123d3354e5f2bffdda0ca5d150a7b437f43c10c
lopo <lopo@zju.edu.cn>
2022-03-14 17:31:39 +0800

    temp: test of toast

--- gh-page/src/components/NavigationMenu.tsx
+++ gh-page/src/components/NavigationMenu.tsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
+import { useToastStore } from "@/scripts/misc";
 
 export default function NavigationMenu() {
+    const useToast = useToastStore()
+
     return (
         <div>
-            Navigation menu <br/>
+            <div className="drag-controller">
+                Navigation menu
+            </div>
+            <button onClick={() => {
+                useToast.info('message')
+            }}>toast</button>
+            <button onClick={() => {
+                useToast.clear()
+            }}>clear</button>
             <Link to="/">home</Link> <br/>
             <Link to="/vite-react-ts">vite-react-ts</Link> <br/>
             <Link to="/vite-vue-ts">vite-vue-ts</Link> <br/>
