c33979892131ed64d51ca4ee32812037b9ef2c3b
lopo <lopo@zju.edu.cn>
2022-03-14 17:13:58 +0800

    feat: layout of home page

--- gh-page/src/views/ZotterPrincipal.tsx
+++ gh-page/src/views/ZotterPrincipal.tsx
@@ -1,25 +1,77 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { UseAxios } from "axios-canceller";
+import { useToastStore } from "@/scripts/misc";
+import { NormalHoverSwitch } from "@/components/NormalHoverSwitch";
+
+export interface Principal {
+    id: string  // uuid
+    number: number  // 1-57
+    commandment: {
+        zh: string
+        en: string
+    }
+    explain: {
+        zh: string
+        en: string
+    }
+}
+
+const PrincipalBox = (prop: { number: number, zh: string, en: string }) => {
+    return (
+        <div style={{
+            position: 'relative',
+            width: 'calc(100% - 102px)',
+            height: '58px',
+            margin: '10px 50px',
+            border: 'solid 1px #cccccc',
+        }}>
+            123
+        </div>
+    )
+}
 
 export const ZotterPrincipal = () => {
     const useAxios = new UseAxios()
 
+    const [ list, setList ] = useState<Principal[]>([])
+
     useEffect(() => {
         useAxios.get('zotter-principal', 'zotter57.json')
             .then((res) => {
-                console.log(res)
+                setList(res.data)
             })
             .catch((err) => {
-                console.log(err)
+                useToastStore().error(err.toString())
             })
         return () => {
             useAxios.cancelScopes('zotter-principal')
         }
-    })
+    }, [])
 
     return (
-        <div>
-            zotter
+        <div style={ {
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#001c33',
+            // display: 'flex',
+            // flexDirection: 'column',
+            // alignItems: 'center',
+            // justifyContent: 'flex-start',
+            overflow: 'auto'
+        } }>
+            { list.map(({ id, number, commandment, explain }) => {
+                return (
+                    <NormalHoverSwitch
+                        key={ id }
+                        normal={
+                            <PrincipalBox number={number} en={commandment.en} zh={commandment.zh}/>
+                        }
+                        hover={
+                            <PrincipalBox number={number} en={commandment.en} zh={commandment.zh}/>
+                        }/>
+                )
+            }) }
         </div>
     )
 }
