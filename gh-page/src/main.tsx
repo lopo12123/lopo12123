38d79e547b14a95165586f0ae98d8f36ed69e5a6
lopo <lopo@zju.edu.cn>
2022-03-14 10:31:54 +0800

    refactor: create routes by map

--- gh-page/src/main.tsx
+++ gh-page/src/main.tsx
@@ -1,8 +1,8 @@
-import {StrictMode} from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
-import App from './App'
+import App from "./App";
 
 import { ReactSpaConverter } from "spa-converter/lib/ReactSpaConverter";
 
@@ -10,26 +10,28 @@ const SpaEntry: string = import.meta.env.DEV
     ? import.meta.env.VITE_APP_SUB_SPA_ENTRY__DEV
     : import.meta.env.VITE_APP_SUB_SPA_ENTRY__PROD
 
+const subAppNameList = [
+    "vite-react-ts",
+    "vite-vue-ts"
+]
+
 ReactDOM.render(
     <StrictMode>
         <HashRouter>
             <Routes>
                 <Route path="/" element={ <App/> }>
-
-                    <Route path="vite-react-ts"
-                           element={
-                               <ReactSpaConverter key="react" entryPath={
-                                   SpaEntry.replace('{SPA_NAME}', 'vite-react-ts')
-                               }/>
-                           }/>
-
-                    <Route path="vite-vue-ts"
-                           element={
-                               <ReactSpaConverter key="vue" entryPath={
-                                   SpaEntry.replace('{SPA_NAME}', 'vite-vue-ts')
-                               }/>
-                           }/>
-
+                    {
+                        subAppNameList.map((name, index) => {
+                            return (
+                                <Route path={ name } key={ index }
+                                       element={
+                                           <ReactSpaConverter key={ name } entryPath={
+                                               SpaEntry.replace('{SPA_NAME}', name)
+                                           }/>
+                                       }/>
+                            )
+                        })
+                    }
                 </Route>
             </Routes>
         </HashRouter>
