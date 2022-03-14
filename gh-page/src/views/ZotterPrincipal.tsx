89afdf3c896ab6b153431a1268f7e2583de3f4a8
lopo <lopo@zju.edu.cn>
2022-03-14 18:23:38 +0800

    feat: enter the content

--- gh-page/src/views/ZotterPrincipal.tsx
+++ gh-page/src/views/ZotterPrincipal.tsx
@@ -16,20 +16,40 @@ export interface Principal {
     }
 }
 
-const PrincipalBox = (prop: { number: number, zh: string, en: string }) => {
+const PrincipalBox = (prop: { number: number, zh: string, en: string, highlight?: boolean }) => {
     return (
-        <div style={{
+        <div style={ {
             position: 'relative',
-            width: 'calc(100% - 202px)',
-            height: '58px',
+            width: 'calc(100% - 204px)',
+            height: '56px',
             margin: '10px 50px',
             padding: '0 50px',
-            border: 'solid 1px #cccccc80',
+            border: `solid 2px ${ prop.highlight ? '#21a0ff80' : '#cccccccc' }`,
+            borderRadius: '5px',
+            backgroundColor: '#01335b',
+            fontSize: '12px',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'flex-start'
-        }}>
-            123
+        } }>
+            <div className="index"
+                 style={ {
+                     width: '150px',
+                     minWidth: '150px',
+                     marginRight: '50px',
+                     fontSize: '18px',
+                     letterSpacing: '3px'
+                 } }>
+                Principle { prop.number }
+            </div>
+            <div className="content">
+                <div className="en">
+                    { prop.en }
+                </div>
+                <div className="zh" style={ { fontFamily: 'cursive' } }>
+                    { prop.zh }
+                </div>
+            </div>
         </div>
     )
 }
@@ -60,17 +80,17 @@ export const ZotterPrincipal = () => {
             backgroundColor: '#001c33',
             color: '#eeeeee',
             fontFamily: 'lab',
-            overflow: 'auto hidden'
+            overflow: 'hidden auto'
         } }>
             { list.map(({ id, number, commandment, explain }) => {
                 return (
                     <NormalHoverSwitch
                         key={ id }
                         normal={
-                            <PrincipalBox number={number} en={commandment.en} zh={commandment.zh}/>
+                            <PrincipalBox number={ number } en={ commandment.en } zh={ commandment.zh }/>
                         }
                         hover={
-                            <PrincipalBox number={number} en={commandment.en} zh={commandment.zh}/>
+                            <PrincipalBox number={ number } en={ explain.en } zh={ explain.zh } highlight={ true }/>
                         }/>
                 )
             }) }
