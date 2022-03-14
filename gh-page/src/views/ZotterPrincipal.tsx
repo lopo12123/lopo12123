23ba687f6653c9c1be05f206f3feaa5ef80caa59
ChenMH <2036604698@qq.com>
2022-03-14 23:57:27 +0800

    style: color set

--- gh-page/src/views/ZotterPrincipal.tsx
+++ gh-page/src/views/ZotterPrincipal.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import { UseAxios } from "axios-canceller";
-import { useToastStore } from "@/scripts/misc";
-import { NormalHoverSwitch } from "@/components/NormalHoverSwitch";
+import {useEffect, useState} from "react";
+import {UseAxios} from "axios-canceller";
+import {useToastStore} from "@/scripts/misc";
+import {NormalHoverSwitch} from "@/components/NormalHoverSwitch";
 
 export interface Principal {
     id: string  // uuid
@@ -18,36 +18,45 @@ export interface Principal {
 
 const PrincipalBox = (prop: { number: number, zh: string, en: string, highlight?: boolean }) => {
     return (
-        <div style={ {
+        <div style={{
             position: 'relative',
-            width: 'calc(100% - 204px)',
-            height: '56px',
-            margin: '10px 50px',
+            width: 'calc(100% - 206px)',
+            height: '54px',
+            margin: '15px 50px',
             padding: '0 50px',
-            border: `solid 2px ${ prop.highlight ? '#21a0ff80' : '#cccccccc' }`,
+            border: `solid 3px ${ prop.highlight ? '#673b47' : '#c0c0c0' }`,
             borderRadius: '5px',
-            backgroundColor: '#01335b',
+            background: `linear-gradient(90deg,
+                #919191 0,
+                #4e4e4e 25%,
+                #5e5e5e 50%,
+                #919191 75%,
+                #c6c6c6 100%
+            )`,
+            boxShadow: '#cccccc 10px 3px 20px',
+            color: prop.highlight ? '#ffffff' : '#777777',
             fontSize: '12px',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'flex-start'
-        } }>
+        }}>
             <div className="index"
-                 style={ {
+                 style={{
                      width: '150px',
                      minWidth: '150px',
                      marginRight: '50px',
+                     color: '#ffffff',
                      fontSize: '18px',
                      letterSpacing: '3px'
-                 } }>
-                Principle { prop.number }
+                 }}>
+                Principle {prop.number}
             </div>
             <div className="content">
                 <div className="en">
-                    { prop.en }
+                    {prop.en}
                 </div>
-                <div className="zh" style={ { fontFamily: 'cursive' } }>
-                    { prop.zh }
+                <div className="zh" style={{fontFamily: 'cursive'}}>
+                    {prop.zh}
                 </div>
             </div>
         </div>
@@ -57,7 +66,7 @@ const PrincipalBox = (prop: { number: number, zh: string, en: string, highlight?
 export const ZotterPrincipal = () => {
     const useAxios = new UseAxios()
 
-    const [ list, setList ] = useState<Principal[]>([])
+    const [list, setList] = useState<Principal[]>([])
 
     useEffect(() => {
         useAxios.get('zotter-principal', 'zotter57.json')
@@ -73,27 +82,26 @@ export const ZotterPrincipal = () => {
     }, [])
 
     return (
-        <div style={ {
+        <div style={{
             position: 'relative',
             width: '100%',
             height: '100%',
-            backgroundColor: '#001c33',
-            color: '#eeeeee',
+            backgroundColor: '#efefef',
             fontFamily: 'lab',
             overflow: 'hidden auto'
-        } }>
-            { list.map(({ id, number, commandment, explain }) => {
+        }}>
+            {list.map(({id, number, commandment, explain}) => {
                 return (
                     <NormalHoverSwitch
-                        key={ id }
+                        key={id}
                         normal={
-                            <PrincipalBox number={ number } en={ commandment.en } zh={ commandment.zh }/>
+                            <PrincipalBox number={number} en={commandment.en} zh={commandment.zh}/>
                         }
                         hover={
-                            <PrincipalBox number={ number } en={ explain.en } zh={ explain.zh } highlight={ true }/>
+                            <PrincipalBox number={number} en={explain.en} zh={explain.zh} highlight={true}/>
                         }/>
                 )
-            }) }
+            })}
         </div>
     )
 }
