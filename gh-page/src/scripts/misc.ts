5c61f9bc9580382f1dc73c3822406a0bba9d1520
lopo <lopo@zju.edu.cn>
2022-03-15 14:31:13 +0800

    feat: fadeout

--- gh-page/src/scripts/misc.ts
+++ gh-page/src/scripts/misc.ts
@@ -7,6 +7,7 @@ class ToastStore {
     bind(newRef: Toast | null) {
         this.ref = newRef
     }
+
     info(msg: string, title?: string, till: number = 3_000) {
         this.ref?.clear()
         this.ref?.show({
@@ -16,6 +17,7 @@ class ToastStore {
             life: till
         })
     }
+
     success(msg: string, title?: string, till: number = 3_000) {
         this.ref?.clear()
         this.ref?.show({
@@ -25,6 +27,7 @@ class ToastStore {
             life: till
         })
     }
+
     warn(msg: string, title?: string, till: number = 3_000) {
         this.ref?.clear()
         this.ref?.show({
@@ -34,6 +37,7 @@ class ToastStore {
             life: till
         })
     }
+
     error(msg: string, title?: string, till: number = 3_000) {
         this.ref?.clear()
         this.ref?.show({
@@ -43,16 +47,52 @@ class ToastStore {
             life: till
         })
     }
+
     clear() {
         this.ref?.clear()
     }
 }
+
 const _toastStore = new ToastStore()
 const useToastStore = () => {
     return _toastStore
 }
+
 // endregion
 
+// region animate
+class Animate {
+    public static fadeOut(el: HTMLElement, type: 'height' | 'width' = 'height', till: number = 3000) {
+        if(!el) return;
+
+        const oriLength = type === 'height' ? el.offsetHeight : el.offsetWidth
+
+        let percent = 1
+        el.style.overflow = 'hidden'
+
+        return new Promise<boolean>((resolve, reject) => {
+            const timer = setInterval(() => {
+                if(percent <= 0) {
+                    clearInterval(timer)
+
+                    el.style[type] = '0'
+                    el.style.opacity = '0'
+                    el.remove()
+
+                    resolve(true)
+                }
+                el.style[type] = oriLength * percent + 'px'
+                el.style.opacity = percent + ''
+                percent -= 0.05
+            }, till / 20)
+        })
+    }
+}
+
+//endregion
+
 export {
-    useToastStore
+    useToastStore,
+
+    Animate
 }
