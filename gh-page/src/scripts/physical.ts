7fcb7642cda0b17f8283af1b79ab4a7f58a116dd
lopo <lopo@zju.edu.cn>
2022-03-15 18:01:13 +0800

    feat: lib for matter-js

--- gh-page/src/scripts/physical.ts
+++ gh-page/src/scripts/physical.ts
@@ -1,16 +1,27 @@
 import { Bodies, Composites, Engine, Render, Runner, World } from "matter-js";
 
+/**
+ * @description
+ * [guide]{@link https://www.jianshu.com/p/00abe24db9bd}
+ */
 class Physical {
     private readonly engine: Engine
     private readonly render: Render
     private readonly world: World
     private runner: (Runner | null) = null
 
-    constructor(canvas: HTMLCanvasElement) {
-        this.engine = Engine.create()  // 生成引擎
+    constructor(canvas: HTMLCanvasElement, width: number = 800, height: number = 600) {
+        this.engine = Engine.create(canvas, {
+            gravity: {},
+            enableSleeping: true
+        })  // 生成引擎
         this.render = Render.create({
             canvas: canvas,
-            engine: this.engine
+            engine: this.engine,
+            options: {
+                width, height,
+                showAngleIndicator: true
+            }
         })
         this.world = this.engine.world
     }
@@ -40,7 +51,7 @@ class Physical {
     runTest() {
         const ground = Bodies.rectangle(200, 300, 100, 20, { isStatic: true })
         const rect = Bodies.rectangle(200, 100, 20, 20)
-        const circle = Bodies.circle(200, 100, 10)
+        const circle = Bodies.circle(200, 100, 10, { restitution: 2 })
         const stack = Composites.stack(200, 100, 1, 5, 0, 10, (x: number, y: number) => {
             return Bodies.circle(x, y, 10)
         })
@@ -48,8 +59,7 @@ class Physical {
         Composites.chain(stack, 0, 0.5, 0, 0.5, {})
 
         const car = Composites.car(200, 100, 100, 10, 10)
-
-        this.addToWorld(ground, car)
+        this.addToWorld(ground, circle)
         this.start()
     }
 }
