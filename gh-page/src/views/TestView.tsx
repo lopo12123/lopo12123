8cb90b1067c6bc22b9f3d2421a82968694266664
lopo <lopo@zju.edu.cn>
2022-03-15 17:21:25 +0800

    test: matter-js start

--- gh-page/src/views/TestView.tsx
+++ gh-page/src/views/TestView.tsx
@@ -1,7 +1,25 @@
+import { useLayoutEffect, useRef } from "react";
+import { Physical } from "@/scripts/physical";
+
 export const TestView = () => {
+    const worldContainer = useRef<HTMLCanvasElement>(null)
+
+    useLayoutEffect(() => {
+        let phy: Physical
+
+        if(!!worldContainer.current) {
+            phy = new Physical(worldContainer.current)
+            phy.runTest()
+        }
+
+        return () => {
+            phy?.stop()
+        }
+    }, [])
+
     return (
-        <div>
-            123
-        </div>
+        <canvas ref={ worldContainer }
+                width={ 400 }
+                height={ 300 }/>
     )
 }
