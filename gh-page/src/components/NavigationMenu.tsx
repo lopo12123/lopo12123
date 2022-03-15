2513e281e1e1c3b60e4bd3e276c6144689276ca8
lopo <lopo@zju.edu.cn>
2022-03-15 15:04:28 +0800

    feat: hide test items

--- gh-page/src/components/NavigationMenu.tsx
+++ gh-page/src/components/NavigationMenu.tsx
@@ -9,15 +9,15 @@ export default function NavigationMenu() {
             <div className="drag-controller">
                 Navigation menu
             </div>
-            <button onClick={() => {
-                useToast.info('message')
-            }}>toast</button>
-            <button onClick={() => {
-                useToast.clear()
-            }}>clear</button>
-            <Link to="/">home</Link> <br/>
-            <Link to="/vite-react-ts">vite-react-ts</Link> <br/>
-            <Link to="/vite-vue-ts">vite-vue-ts</Link> <br/>
+            {/*<button onClick={() => {*/}
+            {/*    useToast.info('message')*/}
+            {/*}}>toast</button>*/}
+            {/*<button onClick={() => {*/}
+            {/*    useToast.clear()*/}
+            {/*}}>clear</button>*/}
+            {/*<Link to="/">home</Link> <br/>*/}
+            {/*<Link to="/vite-react-ts">vite-react-ts</Link> <br/>*/}
+            {/*<Link to="/vite-vue-ts">vite-vue-ts</Link> <br/>*/}
         </div>
     )
 }
