130105f96a91e85e2292fa224238fdd1d30e92e6
lopo <lopo@zju.edu.cn>
2022-03-16 18:10:31 +0800

    docs: add example for bridge generator

--- gh-page/src/scripts/IceAndFire/BasicComponents.ts
+++ gh-page/src/scripts/IceAndFire/BasicComponents.ts
@@ -1,4 +1,4 @@
-import { Bodies, Composites } from "matter-js";
+import { Bodies, Body, Composites, Constraint } from "matter-js";
 
 class BasicComponents {
     /**
@@ -8,13 +8,35 @@ class BasicComponents {
      * @param [boardSize = [30, 20]]
      * @param [boardNumber = 5]
      * @param [borderGap = 10]
+     * @example
+     * const combined = BasicComponents.bridge(...)
+     * World.add(world, combined)
      */
-    public static bridge(startX: number, startY: number, boardSize: [number,number] = [30, 10], boardNumber: number = 5, borderGap: number = 10) {
-        const boards = Composites.stack(startX, startY, boardNumber, 1, borderGap, 0, (x: number, y: number) => {
+    public static bridge(startX: number, startY: number, boardSize: [number,number] = [40, 10], boardNumber: number = 3, borderGap: number = 10) {
+        const left = Bodies.rectangle(startX, startY, 10, 30, { isStatic: true })
+        const right = Bodies.rectangle(startX + boardSize[0] * boardNumber + borderGap * (boardNumber + 1), startY, 10, 30, { isStatic: true })
+
+        const boards = Composites.stack(startX + borderGap, startY, boardNumber, 1, borderGap, 0, (x: number, y: number) => {
             return Bodies.rectangle(x, y, ...boardSize, { chamfer: { radius: 5 } })
         })
+
+        const link_left = Constraint.create({
+            bodyA: left,
+            pointA: { x: 0, y: -5 },
+            bodyB: boards.bodies[0],
+            pointB: { x: -boardSize[0] * 0.4, y: 0  }
+        })
+
         Composites.chain(boards, 0.4, 0, -0.4, 0, {})
-        return boards
+
+        const link_right = Constraint.create({
+            bodyA: boards.bodies[boards.bodies.length - 1],
+            pointA: { x: boardSize[0] * 0.4, y: 0  },
+            bodyB: right,
+            pointB: { x: 0, y: -5 }
+        })
+
+        return [left, link_left, boards, link_right, right ]
     }
 }
 
