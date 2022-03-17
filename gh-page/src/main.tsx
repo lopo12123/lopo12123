a54e6d09e9f429fa8f06979ab73ff0b6cafd67dc
lopo <lopo@zju.edu.cn>
2022-03-17 15:25:17 +0800

    restructure: font

--- gh-page/src/main.tsx
+++ gh-page/src/main.tsx
@@ -3,7 +3,7 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "@szhsin/react-menu/dist/index.css";
-import "@/styles/fonts.scss";
+import "@/fonts/fonts.scss";
 import "@/styles/index.scss";
 
 // core
