5096d7852bb620166a4c629c7c757200965b3150
lopo <lopo@zju.edu.cn>
2022-03-17 10:27:37 +0800

    style: lib style

--- gh-page/src/main.tsx
+++ gh-page/src/main.tsx
@@ -2,6 +2,7 @@
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "@szhsin/react-menu/dist/index.css";
 import "@/styles/fonts.scss";
 import "@/styles/index.scss";
 
