fd423e9ec87e9592621f5f3e5de1d0c7f5f65c7f
ChenMH <2036604698@qq.com>
2022-03-17 23:16:52 +0800

    feat: init place for guide

--- gh-page/src/App.tsx
+++ gh-page/src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
             <Toast ref={ toastRef }/>
 
             <EnableDrag
-                key="test" initPos={ { left: '500px', top: '200px' } }
+                key="test" initPos={ { right: '50px', top: '50px' } }
                 innerEl={ <NavigationMenu/> }/>
 
             <Outlet/>
