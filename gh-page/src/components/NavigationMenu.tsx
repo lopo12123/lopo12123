e677409fb3ad80dc18b5f039dd0f9852acf48dab
lopo <lopo@zju.edu.cn>
2022-03-17 16:25:41 +0800

    feat: tooltip for menu

--- gh-page/src/components/NavigationMenu.tsx
+++ gh-page/src/components/NavigationMenu.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ContextMenu } from "primereact/contextmenu";
-import { useToastStore } from "@/scripts/misc";
+import { Tooltip } from "primereact/tooltip";
 import type { MenuItem } from "primereact/menuitem";
 import { navigateToSubApp, navigateToTool, SubAppNameList, ToolNameList } from "@/router";
+import { useToastStore } from "@/scripts/misc";
 
 export default function NavigationMenu() {
     const navigate = useNavigate()
@@ -64,18 +65,56 @@ export default function NavigationMenu() {
         }
     ]
 
+    const [ inMenu, setInMenu ] = useState(false)
+
     return (
         <div>
             <div className="drag-controller"
+                 onClick={ (e) => {
+                     ctxMenuRef.current?.show(e)
+                 } }
                  onContextMenu={ (e) => {
                      ctxMenuRef.current?.show(e)  // 组件内部有 preventDefault 和 stopPropagation
                  } }>
-                <div className="sprite">
-                    Navigation menu
+                <div id="navigate-menu"
+                     data-pr-tooltip="Place me anywhere."
+                     style={ {
+                         position: 'relative',
+                         width: inMenu ? '80px' : '30px',
+                         height: '30px',
+                         marginLeft: inMenu ? '0' : '25px',
+                         border: 'dashed 1px #777777',
+                         borderRadius: '5px',
+                         display: 'flex',
+                         alignItems: 'center',
+                         justifyContent: 'center',
+                         transition: 'ease 1s',
+                         overflow: 'hidden'
+                     } }
+                     onMouseEnter={ () => {
+                         setInMenu(true)
+                     } }
+                     onMouseLeave={ () => {
+                         setInMenu(false)
+                     } }>
+                    {
+                        inMenu
+                            ? <span style={ {
+                                fontSize: '12px',
+                                fontFamily: 'lab',
+                                whiteSpace: 'nowrap'
+                            } }>
+                                Menu
+                            </span>
+                            : <span className="custom-rgb" style={ { opacity: '0.5' } }>
+                                <i className="pi pi-cog pi-spin" style={ { fontSize: '18px' } }/>
+                            </span>
+                    }
                 </div>
             </div>
 
             <ContextMenu ref={ ctxMenuRef } model={ ctxMenuConfig }/>
+            <Tooltip target="#navigate-menu" mouseTrack mouseTrackTop={20} position="bottom" />
         </div>
     )
 }
