b9f5e758acf9e34c28702d06e615e32a3f3ee864
lopo <2036604698@qq.com>
2022-03-20 17:26:50 +0800

    fix: absolute => relative

--- gh-page/vite.config.ts
+++ gh-page/vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/lopo12123/',
+    base: './',
     plugins: [ react(), vue() ],
     server: {
         port: 8910
