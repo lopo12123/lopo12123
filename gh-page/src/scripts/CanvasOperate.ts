d9dba854b304ebe87850c6779df180cfbc8a4963
lopo <2036604698@qq.com>
2022-03-20 16:08:11 +0800

    feat: merge

--- gh-page/src/scripts/CanvasOperate.ts
+++ gh-page/src/scripts/CanvasOperate.ts
@@ -1,32 +1,9 @@
-import { RefObject } from "react";
 import { fabric } from "fabric";
 import { Misc } from "@/scripts/misc";
-import { useToastStore } from "@/scripts/ToastStore";
-
-class BaseOperate {
-    public static allowType = [ 'jpg', 'png' ]
-
-    public static drawBlobToCanvas(blob: File, canvasRef: RefObject<HTMLCanvasElement>) {
-        if(!!canvasRef.current && !!blob) {
-            const reader = new FileReader()
-            const ctx = canvasRef.current.getContext('2d')!
-            const [ w, h ] = [ canvasRef.current.width, canvasRef.current.height ]
-            reader.readAsDataURL(blob)
-            reader.onload = (e) => {
-                const img = document.createElement('img')
-                img.style.width = '50px'
-                img.style.height = '50px'
-                img.src = e.target!.result as string
-
-                img.onload = () => {
-                    ctx.drawImage(img, 0, 0, w, h)
-                }
-            }
-        }
-    }
-}
 
 class fabricOperate {
+    public static allowType = [ 'jpg', 'png' ]
+
     private canvas: fabric.Canvas
     private grayFilter = new fabric.Image.filters.Grayscale()
     private originWidth: number | undefined
@@ -129,6 +106,5 @@ class fabricOperate {
 }
 
 export {
-    BaseOperate,
     fabricOperate
 }
