c44c1516c7343d22ab1547a8c0cef7f84fed65ad
lopo <lopo@zju.edu.cn>
2022-03-21 16:00:10 +0800

    feat: layout for gojs

--- gh-page/src/views/FlowChart.tsx
+++ gh-page/src/views/FlowChart.tsx
@@ -1,13 +1,46 @@
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
+import { GojsOperate } from "@/scripts/FlowChart.script";
 
 export default () => {
-    const [ aa, setAa ] = useState(1)
+    const paletteContainer = useRef<HTMLDivElement>(null)
+    const diagramContainer = useRef<HTMLDivElement>(null)
+
+    const [ diagramObj, setDiagramObj ] = useState<GojsOperate | null>(null)
+
+    useLayoutEffect(() => {
+        if(!!diagramContainer.current && !!paletteContainer.current) {
+            const instance = new GojsOperate(diagramContainer.current, paletteContainer.current)
+            setDiagramObj(instance)
+        }
+    }, [])
 
     return (
-        <div onClick={ () => {
-            setAa(aa + 1)
-        } }>
-            { aa }
+        <div
+            style={ {
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            } }>
+
+            <div ref={ paletteContainer }
+                 style={ {
+                     position: 'relative',
+                     width: '200px',
+                     height: '100%',
+                     backgroundColor: '#f5f5f5'
+                 } }>
+            </div>
+
+            <div ref={ diagramContainer }
+                 style={ {
+                     position: 'relative',
+                     width: 'calc(100% - 200px)',
+                     height: '100%'
+                 } }>
+            </div>
         </div>
     )
 }
