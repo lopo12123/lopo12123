99b322b157c3f03a91ca436dab82510cf57481c4
lopo <lopo@zju.edu.cn>
2022-03-24 15:52:31 +0800

    style: inspector`s style

--- gh-page/src/views/FlowChart.tsx
+++ gh-page/src/views/FlowChart.tsx
@@ -1,6 +1,8 @@
 import { useLayoutEffect, useRef, useState } from "react";
 import { ContextMenu, ContextMenuOnLoad } from "@/components/FlowChart/ContextMenu";
 import { GojsOperate } from "@/scripts/FlowChart.script";
+import { EnableDrag } from "@/components/misc/EnableDrag";
+import Styles from "@/styles/flowchart.module.scss";
 
 export default () => {
     const diagramContainer = useRef<HTMLDivElement>(null)
@@ -61,17 +63,33 @@ export default () => {
                  } }>
             </div>
 
-            <div ref={ inspectorContainer }
-                 style={ {
-                     position: 'absolute',
-                     zIndex: '200',
-                     width: '200px',
-                     height: '100%',
-                     top: '50px',
-                     right: '50px',
-                     backgroundColor: '#f5f5f5'
-                 } }>
-            </div>
+            <EnableDrag key="inspector-container"
+                        initPos={ { left: '210px', top: '10px' } }
+                        innerEl={
+                            <div style={ {
+                                position: 'relative',
+                                width: '300px',
+                                height: 'fit-content',
+                                border: 'solid 2px #aaaaaa',
+                                borderRadius: '10px',
+                                backgroundColor: '#ffffff',
+                                fontFamily: 'Fira Sans,Arial,sans-serif'
+                            } }>
+                                <div className="drag-controller"
+                                     style={ {
+                                         position: 'relative',
+                                         width: 'calc(100% - 10px)',
+                                         height: '10px',
+                                         margin: '5px',
+                                         borderRadius: '5px',
+                                         backgroundColor: '#aaaaaa'
+                                     } }>
+                                </div>
+                                <div className={ Styles.inspectorPanel }>
+                                    <div ref={ inspectorContainer }/>
+                                </div>
+                            </div>
+                        }/>
 
             <ContextMenu onLoad={ doAfterOnLoad } instance={ diagramObj }/>
         </div>
