50bb76b8b67612700cc27d45c2eda4193e2dde2d
lopo <lopo@zju.edu.cn>
2022-03-28 10:49:45 +0800

    change: change the spa menu list

--- gh-page/packages/shell/src/router/spaNavigate.ts
+++ gh-page/packages/shell/src/router/spaNavigate.ts
@@ -1,97 +1,62 @@
 import { NavigateFunction } from "react-router-dom";
 import type { MenuItem } from "primereact/menuitem";
 
-interface SpaMenu {
-    [groupName: string]: {
-        [spaName: string]: {
-            // label
-            label: string
-            // icon
-            icon: string
-        }
-    }
-}
-const spaMenuConfig: SpaMenu = {
-    tool: {
-        // 'image-parser': {
-        //     label: 'Image Parser',
-        //     icon: 'pi pi-images'
-        // },
-        'flow-chart': {
-            label: 'Flow Chart',
-            icon: 'pi pi-sitemap'
-        }
-    },
-    demo: {
-        'vite-react-ts': {
-            label: 'vite-react-ts',
-            icon: ''
-        },
-        'vite-vue-ts': {
-            label: 'vite-vue-ts',
-            icon: ''
-        }
-    }
-}
-
-export interface SpaMenuItem extends MenuItem{
+export interface SpaMenuItem extends MenuItem {
     path: string
     // label: string
     // icon?: string
     // command?: () => void
     items?: SpaMenuItem[]
 }
-const spaMenuList: SpaMenuItem[] = [
-    {
-        path: 'tool',
-        label: 'tool',
-        icon: 'pi pi-box',
-        items: [
-            // {
-            //     path: 'image-parser',
-            //     label: 'Image Parser',
-            //     icon: 'pi pi-images'
-            // },
-            {
-                path: 'flow-chart',
-                label: 'Flow Chart',
-                icon: 'pi pi-sitemap'
-            }
-        ]
-    },
-    {
-        path: 'spa-demo',
-        label: 'spa demo',
-        icon: 'pi pi-database',
-        items: [
-            {
-                path: 'vite-react-ts',
-                label: 'vite-react-ts',
-                icon: 'pi pi-book'
-            },
-            {
-                path: 'vite-vue-ts',
-                label: 'vite-vue-ts',
-                icon: 'pi pi-book'
-            }
-        ]
-    }
-]
 
-
-/**
- * @description navigate to spa page
- * @param group
- * @param name
- * @param navigate
- */
-const gotoSpa = (group: string, name: string, navigate: NavigateFunction) => {
-    navigate(`${group}/${name}`)
+const useSpaMenu = (navigate?: NavigateFunction): SpaMenuItem[] => {
+    return [
+        {
+            path: 'tool',
+            label: 'tool',
+            icon: 'pi pi-box',
+            items: [
+                // {
+                //     path: 'image-parser',
+                //     label: 'Image Parser',
+                //     icon: 'pi pi-images'
+                // },
+                {
+                    path: 'flow-chart',
+                    label: 'Flow Chart',
+                    icon: 'pi pi-sitemap',
+                    command() {
+                        navigate?.('tool/flow-chart')
+                    }
+                }
+            ]
+        },
+        {
+            path: 'spa-demo',
+            label: 'spa demo',
+            icon: 'pi pi-database',
+            items: [
+                {
+                    path: 'vite-react-ts',
+                    label: 'vite-react-ts',
+                    icon: 'pi pi-book',
+                    command() {
+                        navigate?.('spa-demo/vite-react-ts')
+                    }
+                },
+                {
+                    path: 'vite-vue-ts',
+                    label: 'vite-vue-ts',
+                    icon: 'pi pi-book',
+                    command() {
+                        navigate?.('spa-demo/vite-vue-ts')
+                    }
+                }
+            ]
+        }
+    ]
 }
 
-
 export {
-    // spaMenuConfig,
-    spaMenuList,
-    gotoSpa
+    useSpaMenu
 }
