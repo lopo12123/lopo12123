64c10dc33f1fefe9089230f7efa8dd68994d1b95
lopo <lopo@zju.edu.cn>
2022-03-28 13:39:46 +0800

    refactor: split flow-chart to spa

--- gh-page/packages/flow-chart/src/adaptor.tsx
+++ gh-page/packages/flow-chart/src/adaptor.tsx
@@ -1,7 +1,7 @@
 import { defineSpaApp } from "spa-converter/lib/ReactSpaConverter"
 
 import { render, unmountComponentAtNode } from "react-dom";
-import FlowChart from "@/views/FlowChart";
+import FlowChart from "./views/FlowChart";
 
 export default defineSpaApp((container) => {
     return {
