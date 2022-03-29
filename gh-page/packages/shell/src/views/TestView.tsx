d7d4178fc487162ed0471c3fca2c3003f8b3b72c
lopo <lopo@zju.edu.cn>
2022-03-29 17:54:35 +0800

    test: test view

--- gh-page/packages/shell/src/views/TestView.tsx
+++ gh-page/packages/shell/src/views/TestView.tsx
@@ -1,13 +1,33 @@
-import { spaMenuList } from "@/router/spaNavigate";
+import MidLayer from "@shell/test/MidLayer"
+import EndLayer from "@shell/test/EndLayer"
+import { createContext, useState } from "react";
 
-export const TestView = (prop: { label: string, icon: string }) => {
+export const CtxTest = createContext<any>(null)
 
-    const aa = spaMenuList
+export const TestView = () => {
+    const [ topData, setTopData ] = useState(1)
 
     return (
         <div>
-            label: { prop.label } <br/>
-            icon: { prop.icon }
+            <br/> <br/>
+            <button onClick={ () => {
+                setTopData(topData + 1)
+            } }>
+                do add
+            </button>
+            <br/> <br/>
+            <CtxTest.Provider key="ctx-test" value={topData}>
+                <MidLayer key="layer1" level={ 1 }
+                          innerEl={
+                              <MidLayer key="layer2" level={ 2 }
+                                        innerEl={
+                                            <MidLayer key="layer3" level={ 3 }
+                                                      innerEl={
+                                                          <EndLayer/>
+                                                      }/>
+                                        }/>
+                          }/>
+            </CtxTest.Provider>
         </div>
     )
 }
