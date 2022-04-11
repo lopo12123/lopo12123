b3ee3ec02babdefdd4c172611aea81419c3c714d
lopo <lopo@zju.edu.cn>
2022-04-11 16:29:07 +0800

    feat: load with lazy

--- gh-page/src/main.tsx
+++ gh-page/src/main.tsx
@@ -5,16 +5,36 @@ import "primeicons/primeicons.css";
 import "@/styles/index.scss";
 
 // core
-import { StrictMode } from "react";
+import { lazy, StrictMode, Suspense } from "react";
 import { render } from "react-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { LoadingElement } from "@/components/Misc/LoadingElement";
 
 // view
-import App from "./App";
+const ProjectList = lazy(() => import("@/views/ProjectList"))
 
 // render
 render(
     <StrictMode>
-        <App/>
+        <HashRouter>
+            <Suspense fallback={
+                <div style={ {
+                    position: 'relative',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                } }>
+                    <LoadingElement/>
+                </div>
+            }>
+                <Routes>
+                    <Route path="/" element={ <Navigate to="/project-list"/> }/>
+                    <Route path="/project-list" element={ <ProjectList/> }/>
+                </Routes>
+            </Suspense>
+        </HashRouter>
     </StrictMode>,
     document.getElementById('root')
 )
