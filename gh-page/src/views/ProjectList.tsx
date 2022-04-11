8eec2d2dddd5931c42104b24b2ba95b19fc35815
lopo <lopo@zju.edu.cn>
2022-04-11 17:41:24 +0800

    feat: project list

---
+++ gh-page/src/views/ProjectList.tsx
@@ -0,0 +1,61 @@
+import Styles from "./ProjectList.module.scss";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { useAxios } from "@/stores/useAxiosStore";
+import { useToast } from "@/stores/useToastStore";
+
+interface TableItem {
+    index: number
+    name: string
+    links: {
+        type: string
+        url: string
+    }[]
+}
+
+export default () => {
+    const [ tableData, setTableData ] = useState<TableItem[]>([])
+
+    useLayoutEffect(() => {
+        useAxios()
+            .get('project-list', '/ProjectList.json')
+            .then((res) => {
+                setTableData(res.data)
+            })
+            .catch((e) => {
+                useToast().error(e.toString())
+            })
+
+        return () => {
+            useAxios().cancelScopes('project-list')
+        }
+    }, [])
+
+    const LinkTemplate = ({ links }: TableItem) => {
+        return (
+            <div>
+                {
+                    links.map((link, index) => {
+                        return (
+                            <div key={ index }>{ link.type }</div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+    return (
+        <div className={ Styles.projectList }>
+            <DataTable className={ Styles.table }
+                       value={ tableData }
+                       columnResizeMode="fit"
+                       responsiveLayout="scroll">
+                <Column header="index" field="index" style={ { width: '100px' } }/>
+                <Column header="name" field="name" style={ { width: '100px' } }/>
+                <Column header="links" body={ LinkTemplate }/>
+            </DataTable>
+        </div>
+    )
+}
