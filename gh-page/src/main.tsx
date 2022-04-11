2d831fbd609063e2a057d3d97cbbb127f0e5d7bd
lopo <lopo@zju.edu.cn>
2022-04-11 16:15:42 +0800

    feat: import styles

--- gh-page/src/main.tsx
+++ gh-page/src/main.tsx
@@ -1,7 +1,17 @@
+// style
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "@/styles/index.scss";
+
+// core
 import { StrictMode } from "react";
 import { render } from "react-dom";
+
+// view
 import App from "./App";
 
+// render
 render(
     <StrictMode>
         <App/>
