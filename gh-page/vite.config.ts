b7f9aacd5f6eef0ee3c370e62379a7e114f2e78e
lopo <lopo@zju.edu.cn>
2022-04-11 15:53:37 +0800

    chore: configs

--- gh-page/vite.config.ts
+++ gh-page/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [ react() ],
+    base: './',
+    resolve: {
+        alias: {
+            '@': resolve('./src')
+        }
+    },
+    css: {
+        modules: {
+            localsConvention: 'camelCaseOnly'
+        },
+        preprocessorOptions: {
+            scss: {
+                javascriptEnabled: true
+            }
+        }
+    },
+    server: {
+        port: 7788
+    }
 })
