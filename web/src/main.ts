158fe6e4e5d72aa2f27d66e6d38ea04f7edaae14
lopo <lopo@zju.edu.cn>
2022-05-16 11:16:23 +0800

    feat: base config

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,4 +1,7 @@
 // style
+import "primevue/resources/themes/saga-blue/theme.css"  //theme
+import "primevue/resources/primevue.min.css"            //core css
+import "primeicons/primeicons.css"                      //icons
 import "@/styles/index.scss";
 
 // core
@@ -6,10 +9,14 @@ import { createApp } from "vue";
 
 // plugin
 import { router } from "@/router";
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 
 // view
 import App from "./App.vue";
 
 createApp(App)
     .use(router)
+    .use(PrimeVue)
+    .use(ToastService)
     .mount('#app')
