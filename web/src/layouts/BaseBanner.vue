7dec3371c89c33e4d2c8d49ed7e8aef1d3765885
lopo <lopo@zju.edu.cn>
2022-05-16 17:48:53 +0800

    feat: base banner

--- web/src/layouts/BaseBanner.vue
+++ web/src/layouts/BaseBanner.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 import Breadcrumb from "primevue/breadcrumb";
+import TieredMenu from "primevue/tieredmenu";
+import { MenuItem } from "primevue/menuitem";
 
 const route = useRoute()
+
+// region bread
 const bread_home = {
     icon: 'pi pi-home',
     to: { name: 'Home' }
@@ -11,10 +15,39 @@ const bread_home = {
 const bread_other = computed(() => {
     return route.meta.bread ?? []
 })
+// endregion
+
+// region tiered
+const tiered_ref = ref<TieredMenu | null>(null)
+const tiered_items: MenuItem[] = [
+    {
+        label: 'Home',
+        icon: 'pi pi-home',
+    },
+    {
+        separator: true
+    },
+    {
+        label: 'Github',
+        icon: 'pi pi-pencil'
+    },
+    {
+        label: 'Npm',
+        icon: 'pi pi-pencil'
+    }
+]
+const tiered_toggle = (e: MouseEvent) => {
+    tiered_ref.value?.toggle(e)
+}
+// endregion
 </script>
 
 <template>
     <div class="base-banner">
+        <div class="tiered-container" @click="tiered_toggle">
+            <img class="lopo-blink" src="../assets/icon.png" alt="">
+            <TieredMenu ref="tiered_ref" :popup="true" :model="tiered_items"/>
+        </div>
 
         <div class="bread-container">
             <Breadcrumb :home="bread_home" :model="bread_other"/>
@@ -28,5 +61,29 @@ const bread_other = computed(() => {
     width: 100%;
     height: calc(100% - 1px);
     border-bottom: solid 1px #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .tiered-container {
+        position: relative;
+        width: 59px;
+        height: 24px;
+        border-right: solid 1px #ccc;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        img {
+            position: relative;
+            width: 24px;
+            height: 24px;
+        }
+    }
+
+    .bread-container {
+        position: relative;
+        width: calc(100% - 60px);
+    }
 }
 </style>
