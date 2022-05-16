5620a2d7585ef396d9a1ffed8b1e2982e9b00b54
lopo <lopo@zju.edu.cn>
2022-05-16 17:31:46 +0800

    feat: d3 clock

--- web/src/layouts/D3Clock.vue
+++ web/src/layouts/D3Clock.vue
@@ -34,25 +34,39 @@ const draw_d3_clock = (w: number, h: number) => {
         .append('line')
         .attr('x1', 1)
         .attr('y1', 0)
-        .attr('x2', (d, i) => {
-            // 0 15 30 45 刻度加粗
-            return i % 15 === 0 ? 10 : 6
-        })
+        .attr('x2', 6)
         .attr('y2', 0)
         // 边框颜色、角度
         .attr('transform', (d, i) => {
-            // 角度: 右侧为0, 顺时针
             return `rotate(${ 6 * i - 90 }) translate(${ radius - 10 })`
         })
         // 刻度颜色
         .style('stroke', (d, i) => {
-            return i % 15 === 0 ? '#ffa500' : '#7fae02'
+            return i % 15 === 0 ? '#7fae02' : '#ffa500'
         })
         // 刻度宽度
-        .style('stroke-width', 3)
+        .style('stroke-width', (d, i) => {
+            return i % 15 === 0 ? 3 : 2
+        })
     // endregion
 
     // region 时分秒 pointer
+    // region 图例
+    const text = svg
+        .append('g')
+        .selectAll('.label-text')
+        .data([ [ 'H', '#ec4117' ], [ 'M', '#2da0ff' ], [ 'S', '#001c33' ] ])
+        .enter()
+        .append('text')
+        .attr('class', 'label-text')
+        .attr('transform', (d, i) => {
+            return `translate(${ radius - 20 + i * 20 }, ${ radius + 5 })`
+        })
+        .text((p) => p[0])
+        .style('font-size', '12px')
+        .style('fill', (p) => p[1])
+        .style('text-anchor', "middle")
+    // endregion
     // region 时针
     const hour = svg
         .append('g')
@@ -62,10 +76,10 @@ const draw_d3_clock = (w: number, h: number) => {
         .append('line')
         .attr('x1', 0)
         .attr('y1', 0)
-        .attr('x2', 5)
+        .attr('x2', 3)
         .attr('y2', 0)
         .style('stroke', '#ec4117')
-        .style('stroke-width', '5')
+        .style('stroke-width', 3)
     // endregion
     // region 分针
     const min = svg
@@ -76,10 +90,10 @@ const draw_d3_clock = (w: number, h: number) => {
         .append('line')
         .attr('x1', 0)
         .attr('y1', 0)
-        .attr('x2', 5)
+        .attr('x2', 3)
         .attr('y2', 0)
         .style('stroke', '#2da0ff')
-        .style('stroke-width', '5')
+        .style('stroke-width', 3)
     // endregion
     // region 秒针
     const sec = svg
@@ -90,10 +104,10 @@ const draw_d3_clock = (w: number, h: number) => {
         .append('line')
         .attr('x1', 0)
         .attr('y1', 0)
-        .attr('x2', 5)
+        .attr('x2', 3)
         .attr('y2', 0)
         .style('stroke', '#001c33')
-        .style('stroke-width', '5')
+        .style('stroke-width', 3)
     // endregion
 
     const update_pointers = () => {
@@ -108,11 +122,11 @@ const draw_d3_clock = (w: number, h: number) => {
         })
         min.attr('transform', () => {
             // 角度: 右侧为0, 顺时针
-            return `rotate(${ val_min * 6 - 90 }) translate(${ radius - 30 })`
+            return `rotate(${ val_min * 6 - 90 }) translate(${ radius - 25 })`
         })
         hour.attr('transform', () => {
             // 角度: 右侧为0, 顺时针
-            return `rotate(${ val_hour * 30 - 90 }) translate(${ radius - 40 })`
+            return `rotate(${ val_hour * 30 - 90 }) translate(${ radius - 30 })`
         })
     }
 
