ce9892b0c18d263f45f0a675c1bbb380598d2427
lopo <lopo@zju.edu.cn>
2022-05-16 17:13:49 +0800

    feat: test view

---
+++ web/src/views/Test.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import D3Clock from "@/layouts/D3Clock.vue";
+</script>
+
+<template>
+    <div class="test">
+        test <br>
+        <div style="width: 200px; height: 200px">
+<!--        <div style="width: 600px; height: 400px">-->
+            <D3Clock/>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.test {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
