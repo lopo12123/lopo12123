c385751b78ce07ed358c0d6a37729fd82c93fb6f
lopo <lopo@zju.edu.cn>
2022-05-16 17:13:26 +0800

    feat: router - meta and test page

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,17 +1,35 @@
 import Home from "@/views/Home.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw, RouterOptions } from "vue-router";
 
+declare module 'vue-router' {
+    interface RouteMeta {
+        bread: {
+            label: string,
+            icon?: string,
+            to: string | { name: string }
+        }[]
+    }
+}
+
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
-        redirect: {
-            name: 'Home'
-        }
+        name: 'Home',
+        component: Home,
+        meta: { bread: [] }
     },
     {
-        path: '/home',
-        name: 'Home',
-        component: Home
+        path: '/test',
+        name: 'Test',
+        component: () => import("@/views/Test.vue"),
+        meta: {
+            bread: [
+                {
+                    label: 'test',
+                    to: { name: 'Test' }
+                }
+            ]
+        }
     }
 ]
 
