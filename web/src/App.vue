f3abec3bff2c0321b512f00502a4d0da4b312d12
lopo <lopo@zju.edu.cn>
2022-05-16 11:16:29 +0800

    feat: base layout

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,45 @@
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import BaseBanner from "@/layouts/BaseBanner.vue";
+import Toast from "primevue/toast";
+import { useToast } from "primevue/usetoast";
+import { useToastStore } from "@/stores/store_toast";
+
+const toast = useToast()
+onMounted(() => {
+    useToastStore().bind(toast)
+})
+</script>
+
 <template>
     <div class="app">
-        app root
-        <router-view/>
+        <Toast/>
+
+        <div class="banner-container">
+            <BaseBanner/>
+        </div>
+        <div class="view-container">
+            <router-view/>
+        </div>
     </div>
 </template>
+
+<style lang="scss">
+.app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    .banner-container {
+        position: relative;
+        width: 100%;
+        height: 60px;
+    }
+
+    .view-container {
+        position: relative;
+        width: 100%;
+        height: calc(100% - 60px);
+    }
+}
+</style>
