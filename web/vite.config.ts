19e386c35ea564c82d5bb1dbf2ac2f3fe308b815
lopo <lopo@zju.edu.cn>
2022-05-17 17:29:39 +0800

    chore: build config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,19 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
+    build: {
+        outDir: '../docs'
+        // rollupOptions: {
+        //     output: {
+        //         manualChunks: (id) => {
+        //             if(id.includes('node_modules')) {
+        //                 return (id.toString().split('node_modules/')[1].split('/')[0])
+        //             }
+        //         }
+        //     }
+        // }
+    },
     plugins: [ vue() ],
     resolve: {
         alias: {
