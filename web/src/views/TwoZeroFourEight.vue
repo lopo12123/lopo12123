bbb1be2f73df274e29e367c3f9a655395d626d88
lopo <lopo@zju.edu.cn>
2022-05-18 17:55:36 +0800

    feat: view of 2048

---
+++ web/src/views/TwoZeroFourEight.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import { Two_zero_four_eight } from "@/scripts/two_zero_four_eight";
+import { ref } from "vue";
+
+const game = new Two_zero_four_eight()
+const gridState = ref(game.have_a_look())
+</script>
+
+<template>
+    <div class="two-zero-four-eight">
+        <div class="banner-area">
+
+        </div>
+        <div class="game-area">
+            <div class="grid-container">
+                <div class="row" :key="'row-'+row_idx"
+                     v-for="(row, row_idx) in gridState">
+                    <div class="column" :key="'col-'+col_idx"
+                         v-for="(col, col_idx) in row">
+                        {{ col }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.two-zero-four-eight {
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    .banner-area {
+        position: relative;
+        width: 100%;
+        height: 100px;
+    }
+
+    .game-area {
+        position: relative;
+        width: 100%;
+        height: calc(100% - 100px);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .grid-container {
+            position: relative;
+            width: fit-content;
+            height: fit-content;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+}
+</style>
