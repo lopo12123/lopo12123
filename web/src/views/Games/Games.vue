161ac1751d1ecb3dff4d26740a6a33e56013cff0
lopo <lopo@zju.edu.cn>
2022-05-18 21:34:12 +0800

    feat: game and 2048 page

---
+++ web/src/views/Games/Games.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="games">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.games {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
