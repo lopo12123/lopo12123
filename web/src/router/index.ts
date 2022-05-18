c2209d9abce1992372f07f288e1c01fa3d0c5b7c
lopo <lopo@zju.edu.cn>
2022-05-18 17:55:31 +0800

    feat: router of 2048

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -19,6 +19,19 @@ const routes: RouteRecordRaw[] = [
         meta: { bread: [] }
     },
     {
+        path: '/two-zero-four-eight',
+        name: 'TwoZeroFourEight',
+        component: () => import("@/views/TwoZeroFourEight.vue"),
+        meta: {
+            bread: [
+                {
+                    label: '2048',
+                    to: { name: 'TwoZeroFourEight' }
+                }
+            ]
+        }
+    },
+    {
         path: '/test',
         name: 'Test',
         component: () => import("@/views/Test.vue"),
