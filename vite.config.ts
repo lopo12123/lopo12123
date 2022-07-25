7a444d3b5ed29cfc94d2c65d865e270f50023a79
chenmh <chenmh@sucsoft.com>
2022-07-25 09:55:49 +0800

    refactor: rebuild the file tree

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
+    root: './src/index.html',
     build: {
         emptyOutDir: true,
-        outDir: '../docs',
+        outDir: './docs',
         // rollupOptions: {
         //     output: {
         //         manualChunks: (id) => {
