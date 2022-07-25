46a64247e40ba2283c30fef9fda7c5174a0e7cb5
chenmh <chenmh@sucsoft.com>
2022-07-25 10:07:06 +0800

    chore: base

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="index-view">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.index-view {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
