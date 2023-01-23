00b27272bd9517ef34b8c6a5f0af782bf5831b4c
lopo <lopo@zju.edu.cn>
2023-01-23 21:29:52 +0800

    feat: nonsense

--- src/pages/MyProjects.vue
+++ src/pages/MyProjects.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
-
+const nonsense = "Listen to me, take your time. I know you're in a hurry, but don't be in a hurry."
 </script>
 
 <template>
     <div class="my-projects">
-        MyProjects
+        {{ nonsense }}
     </div>
 </template>
 
@@ -13,5 +13,11 @@
     position: relative;
     width: 100%;
     height: 100%;
+    padding: 32px;
+    font-family: cursive;
+    font-size: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
