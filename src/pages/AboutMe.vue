b951826c50b8d5ab07fc6d0c7318df1e698aaa8d
lopo <lopo@zju.edu.cn>
2023-01-23 21:23:30 +0800

    feat: quote

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -9,6 +9,8 @@ const bannerCfg = {
         window.open('mailto:lopo@zju.edu.cn?subject=Matters about {Write Your Purpose Here}, from viewers from your homepage.')
     }
 }
+
+const quoteText = "If you're a plumber, you're better off learning how to use an ax by chopping down trees with it than unclogging gutters."
 </script>
 
 <template>
@@ -18,7 +20,7 @@ const bannerCfg = {
         <div class="padding">
             <div class="title">
                 <div class="name">lopo12123</div>
-                <div class="quote">123</div>
+                <div class="quote">"{{ quoteText }}"</div>
             </div>
 
             <div class="body">
@@ -48,6 +50,13 @@ const bannerCfg = {
                 font-size: 48px;
                 font-weight: bold;
                 font-family: "Curlz MT", sans-serif;
+            }
+
+            .quote {
+                margin-top: 16px;
+                font-style: italic;
+                font-family: cursive;
+                text-indent: 2em;
             }
         }
 
