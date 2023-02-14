b0281006b50aae5375601f8be1e2487b364ddf44
lopo <lopo@zju.edu.cn>
2023-02-14 23:55:43 +0800

    style: icons

---
+++ src/constants/IconLogo.ts
@@ -0,0 +1,9 @@
+export type IconPlatform = 'android' | 'nodejs'
+
+export type IconFrame = 'vue' | 'react' | 'flutter'
+
+export type IconLanguage = 'js' | 'ts'
+    | 'rust' | 'dart' | 'java' | 'markdown'
+    | 'json' | 'toml'
+
+export type IconLogo = IconPlatform | IconFrame | IconLanguage
