f7a348dd408893020910b5f83b42182960a19ffd
lopo <lopo@zju.edu.cn>
2023-07-11 00:03:36 +0800

    todo: contextmenu with global doesnot work

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Suspense} from 'react'
 import {Outlet} from 'react-router-dom'
 import {PageLoading} from '@/pages/_loading/loading'
 import DockBar from '@/layouts/dock/dock'
+import GlobalMenu from "@/layouts/global_menu/global_menu";
 
 function App() {
     return (
@@ -9,7 +10,9 @@ function App() {
             <div style={{width: '100%', height: '100%'}}>
                 <Suspense fallback={<PageLoading/>} children={<Outlet/>}/>
             </div>
+
             <DockBar/>
+            <GlobalMenu/>
         </>
     )
 }
